df617d16c2f2226d920bdd09594ff6689a3edeff
Leon van den Beukel <leon@vandenbeukel.net>
2020-03-23 19:09:56 +0100

    First commit

---
+++ Distance-Warner.ino
@@ -0,0 +1,53 @@
+/* 
+ 
+  Distance Warning device using an HC-SR04 sensor
+
+  The distance is set to 180 cm or 6 feet.
+  
+*/
+
+const byte trigPin = 9;
+const byte echoPin = 10;
+const byte ledRedPin = 4;
+const byte ledGreenPin = 5;
+const byte speakerPin = 8;
+const int toneFrequency = 2000;
+int maxDistance = 180;   // EU countries mostly prescribe 1.5 meters other countries prescribe 6 feet (180 cm)
+int distance;
+long duration;
+
+void setup() {
+  
+  pinMode(trigPin, OUTPUT); 
+  pinMode(echoPin, INPUT); 
+  pinMode(ledRedPin, OUTPUT);
+  pinMode(ledGreenPin, OUTPUT);
+
+  // Serial.begin(9600); 
+}
+
+void loop() {
+
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  duration = pulseIn(echoPin, HIGH);  
+  distance= duration*0.034/2;
+  
+  // Serial.println(distance);
+  
+  if (distance < maxDistance) {
+    tone(speakerPin, toneFrequency);
+    digitalWrite(ledRedPin, HIGH);
+    digitalWrite(ledGreenPin, LOW);
+  } else {
+    noTone(speakerPin);
+    digitalWrite(ledRedPin, LOW);
+    digitalWrite(ledGreenPin, HIGH);  
+  }
+
+}
